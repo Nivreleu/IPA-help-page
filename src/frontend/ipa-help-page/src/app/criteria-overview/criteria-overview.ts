f6a0155a8f5372585f3bb44dba75033f84aaf34c
cxs1mon <caroline.simon@gmx.ch>
2025-12-15 16:57:55 +0100

    change to standalone components

---
+++ src/frontend/ipa-help-page/src/app/criteria-overview/criteria-overview.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-criteria-overview',
+  standalone: true,
+  templateUrl: './criteria-overview.html',
+  styleUrl: './criteria-overview.scss',
+})
+export class CriteriaOverview {
+
+}
